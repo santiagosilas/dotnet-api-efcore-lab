15eb2d484b26fb14012ba7b6a1de2aeb36bc8b6c
Silas Santiago <santiagosilas@outlook.com>
2025-11-13 12:27:02 -0300

    Exemplo Simples Integração Web--Console

---
+++ backend/client-prompt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+// using System.Net.Http.Json;
+
+var client = new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7181")
+};
+
+var forecasts = await client.GetFromJsonAsync<WeatherForecast[]>("/WeatherForecast");
+
+Console.WriteLine("Weather Forecasts:");
+foreach (var forecast in forecasts!)
+{
+    Console.WriteLine($"{forecast.Date:yyyy-MM-dd} - {forecast.TemperatureC}°C - {forecast.Summary}");
+}
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+}    
