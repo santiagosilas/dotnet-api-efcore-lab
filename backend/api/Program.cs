aadcd7818ab9638ee374d8de17a65bbbccebeb92
Silas Santiago <santiagosilas@outlook.com>
2025-11-13 12:29:59 -0300

    Atualizações em Program.cs e outros arquivos..

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,3 +1,7 @@
+using api.Data;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 string? stcnn = builder.Configuration.GetConnectionString("SqliteConnection");
@@ -16,6 +20,34 @@ builder.Services.AddEndpointsApiExplorer(); // Swagger/OpenAPI - https://aka.ms/
 builder.Services.AddSwaggerGen();
 // builder.Services.AddOpenApi();  // Vem por padrão ao criar o projeto
 
+
+// Adiciona o contexto do bd ao contêiner de serviços (DI - Dependency Injection)
+// builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("MemoryDb"));
+builder.Services.AddDbContext<ApiContext>(opt => opt.UseSqlite(stcnn));
+// builder.Services.AddDbContext<ApiContext>(opt => opt.UseNpgsql(stcnn2));
+
+// Adicionar politica de CORS (Cross-Origin Resource Sharing)
+// É um mecanismo de segurança implementado pelos navegadores
+// que controla se uma aplicação web em um domínio (por exemplo,
+// http://frontend.com.br) pode fazer requisições para um servidor
+// em outro domínio http://api.backend.com)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:5173",
+                "https://localhost:5173"
+                )
+                  .AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+                  
+        });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,9 +56,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     // app.MapOpenApi();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<ApiContext>();
+        context.PopulateTestData();
+    }
+
 }
 
 app.UseHttpsRedirection();
+
+// Permitir CORS
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
